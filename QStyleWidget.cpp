c0c4c05f948f6e607f310a999a092437a01def90
mingyou <13659216860@163.com>
2018-09-14 11:19:54 +0800

    修改 |

--- QStyleWidget.cpp
+++ QStyleWidget.cpp
@@ -1,11 +1,11 @@
 #include "QStyleWidget.h"
 
 #include <QCheckBox>
-
+#include <QLabel>
 QStyleWidget::QStyleWidget(QWidget *parent)
     : QWidget(parent)
 {
-    m_checkBox = new QCheckBox(this);
+      m_checkBox = new QCheckBox(this);
 }
 
 QStyleWidget::~QStyleWidget()
